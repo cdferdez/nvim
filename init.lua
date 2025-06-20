7525b2d79f42965650361880e5ac8957dd6456ee
cdf <cdferdez@gmail.com>
2025-06-20 01:58:47 -0700

    update theme to deep ocean

--- init.lua
+++ init.lua
@@ -790,25 +790,17 @@ require('lazy').setup({
     },
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+  { -- Material.nvim colorscheme
+    'marko-cerovac/material.nvim',
+    priority = 1000, -- Ensure it loads before other plugins
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- Set your preferred material style: 'darker', 'lighter', 'oceanic', 'palenight', 'deep ocean'
+      vim.g.material_style = 'deep ocean'
+      require('material').setup({
+        -- Minimal config, see :help material.nvim for more options
+        -- You can add plugin integrations or style overrides here
+      })
+      vim.cmd.colorscheme 'material'
     end,
   },
 
